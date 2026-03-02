034a22c21c2a2f71d364264c7cbb163fde2824c5
Satyam Umrao <satyamumrao11225@gmail.com>
2026-03-02 12:51:39 +0530

    fix

--- 3.printFunctions/print.go
+++ 3.printFunctions/print.go
@@ -3,46 +3,38 @@ package main
 import "fmt"
 
 func main() {
+
+	// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+	//                                                                                           //
+	//                         printing methods in Golang (fmt package)                          //
+	//                                                                                           //
+	// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+	//                                                                                           //
+	//    |<<------------------method------------------|-----------purpose------------>>|        //
+	//    |     fmt.Println()                          | adds automatic new line       |        //
+	//    |     fmt.Printf()                           | uses format specifiers        |        //
+	//    |     fmt.Print()                            | no auto new line              |        //
+	//    |<<------------------------------------------|-------------------------------->>|        //
+	//                                                                                           //
+	// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+
 	name := "Satyam"
 	age := 22
 	height := 5.97
 
-	// println :
-	// automatically add a new line at the end of the output
+	// <<<-------using Println------->>>
 	fmt.Println("<<<-------using Println------->>>")
 	fmt.Println("age is", age)
 	fmt.Println("name is", name)
 	fmt.Println("height is", height)
 
-	// printf
-	// required format specifiers like %d ,%s etc.
+	// <<<-------using Printf------->>>
 	fmt.Print("\n<<<-------using Printf------->>>\n")
 	fmt.Printf("name is %s\n", name)
 	fmt.Printf("age is %d\n", age)
 	fmt.Printf("height is %.2f\n", height)
 
-	// In printf and print if we want to change the line ,
-	// we add \n to change the line
-
-	// print function without
-	// line change (ln) in println
-	// or formatter (f) in printf
+	// <<<-------Without formatter------->>>
 	fmt.Print("\n<<<-------Without formatter------->>>")
 	fmt.Print("\nage is ", age)
-
 }
-
-// Output
-//
-// <<<-------using Println------->>>
-// age is 22
-// name is Satyam
-// height is 5.97
-
-// <<<-------using Printf------->>>
-// name is Satyam
-// age is 22
-// height is 5.97
-
-// <<<-------Without formatter------->>>
-// age is 22
