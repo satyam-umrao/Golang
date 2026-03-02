8d0b0d3a8eb90c2d90f130cd378bfdda428edf19
Satyam Umrao <satyamumrao11225@gmail.com>
2026-03-02 12:43:10 +0530

    code added

---
+++ 3.printFunctions/specifiers.go
@@ -0,0 +1,12 @@
+package main
+
+func format() {
+	// format specifiers for printf in Golang
+	//    |<<--------------for----------------|---specifiers--->>|
+	//    |		for integer type values       |     %d		     |
+	//    |     for float type values		  |     %.2f		 |  .2 for 2 digit after (.)
+	//    |     for string type values        |     %s		     |
+	//    |     for checking type of values   |     %T           |
+	//    |    for changing line              |     \n           |  not a specifier
+	//    |<<---------------------------------|------------------|
+}
