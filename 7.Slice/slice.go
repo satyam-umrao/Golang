5a7236bda2b905636ee063141cdcbdb68ac73707
Satyam Umrao <satyamumrao11225@gmail.com>
2026-03-02 21:10:55 +0530

    code added

--- 7.Slice/slice.go
+++ 7.Slice/slice.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+// Main function
+func main() {
+	// slice creation method 1
+	// syntax: []dType{values}
+	slice1 := []int{1, 2, 3, 4, 5}
+
+	// slice creation method 2
+	// syntax: make([]dType, length)
+	slice2 := make([]int, 5) // creates a slice of length 5 with default values (0)
+
+	// slice creation method 3
+	// synatx: make([]dType, length, capacity)
+	slice3 := make([]int, 0, 5) // creates a slice with length 0 and capacity 5
+
+	// printing the slices
+	fmt.Println("Slice 1 : ", slice1)
+	fmt.Println("Slice 2 : ", slice2)
+	fmt.Println("Slice 3 : ", slice3)
+
+	fmt.Println("slice 3 length: ", len(slice3))
+	fmt.Println("slice 3 capacity: ", cap(slice3))
 }
