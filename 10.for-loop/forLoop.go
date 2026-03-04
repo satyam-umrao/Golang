12413954b96bbef1e88c3a6926e18b3b3176d90f
Satyam Umrao <satyamumrao11225@gmail.com>
2026-03-04 16:43:27 +0530

    code added

--- 10.for-loop/forLoop.go
+++ 10.for-loop/forLoop.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+// for loop
+func SimpleForLoop() {
+	for i := 0; i < 5; i++ {
+		println(i)
+	}
+}
 
+// infinite for loop
+func InfiniteForLoop() {
+	for {
+		println("Infinite loop")
+	}
+}
+
+// for loop with break statement
+func ForLoopWithBreak() {
+	for i := 0; i < 10; i++ {
+		println(i)
+		if i == 5 {
+			break
+		}
+	}
+}
+
+// for loop in slice
+func ForLoopInSlice() {
+	slice := []string{"Go", "Python", "Java", "C++"}
+	for index, value := range slice {
+		println(index, value)
+	}
+}
+
+// for loop in string
+func ForLoopInString() {
+	str := "Hello, World!"
+	for index, char := range str {
+		println(index, string(char))
+	}
+}
+
+// main function
 func main() {
-	fmt.Println("Hello, World!")
+	println("Simple For loop")
+	SimpleForLoop()
+
+	println("Infinite for loop")
+	// InfiniteForLoop()
+
+	println("ForLoop With Break")
+	ForLoopWithBreak()
+
+	println("ForLoop In Slice")
+	ForLoopInSlice()
+
+	println("ForLoop In String")
+	ForLoopInString()
 }
