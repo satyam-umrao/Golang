63be4c1d0d8747c6c48f7f6c31ea55ec7fdc5d23
Satyam Umrao <satyamumrao11225@gmail.com>
2026-03-07 22:55:02 +0530

    code added

--- 16.timePackage/time.go
+++ 16.timePackage/time.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+	fmt.Println("-------------Time package in Go------------")
+	CurrentTime := time.Now()
+	fmt.Println(CurrentTime.String())
+
+	fmt.Println("\n--------------Time Formatting--------------")
+	formattedTime := CurrentTime.Format("2006-01-02 03:04:05 PM Monday")
+	fmt.Println(formattedTime)
+
+	fmt.Println("\n-------------Time Parsing-----------------")
+	timeString := "2024-06-01 12:30:00"
+	parsedTime, _ := time.Parse("2006-01-02 15:04:05", timeString)
+	fmt.Println(parsedTime)
+
+	fmt.Println("\n-------------Time Duration----------------")
+	duration := time.Hour*2 + time.Minute*30
+	fmt.Println("Duration:", duration)
 
 }
