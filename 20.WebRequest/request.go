8964e6abe05d7d01ea4b989caddc43603a3bc621
Satyam Umrao <satyamumrao11225@gmail.com>
2026-03-26 21:36:56 +0530

    code added

--- 20.WebRequest/request.go
+++ 20.WebRequest/request.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
+func main() {
+	fmt.Println("Web Request in Golang")
+	res, err := http.Get("https://jsonplaceholder.typicode.com/todos/2")
+	if err != nil {
+		fmt.Printf("error while making request: %v", err)
+		return
+	}
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("error while reading response: %v", err)
+		return
+	}
+	fmt.Println(string(data))
 }
