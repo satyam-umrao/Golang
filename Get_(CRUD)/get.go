5cace2c3bfe5aa84b9f4c3c79a378925c2e02e58
Satyam Umrao <satyamumrao11225@gmail.com>
2026-03-27 22:35:01 +0530

    code added

---
+++ Get_(CRUD)/get.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Todo struct {
+	UserId    int    `json:"userId"`
+	Id        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+func main() {
+	res, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+	if err != nil {
+		fmt.Println("Error getting Url : ", err)
+		return
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		fmt.Println("Error Code : ", res.Status)
+		return
+	}
+
+	// data, err := ioutil.ReadAll(res.Body)
+	// if err != nil {
+	// 	fmt.Println("Error getting data : ", err)
+	// 	return
+	// }
+	// fmt.Println("Data : ", string(data))
+
+	var todo Todo
+	err = json.NewDecoder(res.Body).Decode(&todo) // decode
+	if err != nil {
+		fmt.Println("error :", err)
+		return
+	}
+	fmt.Println("Todo", todo)
+}
