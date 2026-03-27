f46afb79e7c612c274124e6241f4ed396c141d51
Satyam Umrao <satyamumrao11225@gmail.com>
2026-03-27 23:16:40 +0530

    fix

--- 24.PostMethod/tempCodeRunnerFile.go
+++
@@ -1,6 +0,0 @@
-response, err := ioutil.ReadAll(res.Body)
-	// if err != nil {
-	// 	fmt.Println("Error in Reading Response : ", err)
-	// 	return
-	// }
-	// fmt.Println("response : ", string(response))
